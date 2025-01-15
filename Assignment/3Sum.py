be8980a748b0b67c7a6870cb8b9cf6cdaafbb3b4
Sahil Shah <ss9656484@gmail.com>
2025-01-15 16:49:57 +0530

    q3 and q4

---
+++ Assignment/3Sum.py
@@ -0,0 +1,4 @@
+# Prompt the user for two numbers and print their sum.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print("Sum of given two numbers is " + str(num1 + num2))
---
+++ Assignment/4SimpleCal.py
@@ -0,0 +1,7 @@
+# Prompt the user for two numbers and display their sum, difference, product, and quotient.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+print("Sum of given two numbers is " + str(num1 + num2))
+print("Difference of given two numbers is " + str(num1 - num2))
+print("Product of given two numbers is " + str(num1 * num2))
+print("Quotient of given two numbers is " + str(num1 / num2))
