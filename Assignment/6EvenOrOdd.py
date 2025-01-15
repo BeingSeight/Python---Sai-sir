6299ffed9643c5dd3faff8197ee69db12b4aa37e
Sahil Shah <ss9656484@gmail.com>
2025-01-15 17:00:11 +0530

    q6

---
+++ Assignment/6EvenOrOdd.py
@@ -0,0 +1,7 @@
+# Prompt the user for an integer and print whether it’s even or odd.
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
