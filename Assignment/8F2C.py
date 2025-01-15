84f8328aefbf66b420da0d152bc7cb9fc5991c4e
Sahil Shah <ss9656484@gmail.com>
2025-01-15 19:34:04 +0530

    q8

--- Assignment/8F2C.py
+++ Assignment/8F2C.py
@@ -1,1 +1,4 @@
 # Convert a given temperature in Fahrenheit to Celsius and print the result.
+temp = float(input("Enter the temperature in Fahrenheit: "))
+temp = (temp - 32) * 5/9
+print("The temperature in Celsius is " + str(temp))
